b84b3ac174d63d84978c288089f2db46d7eff524
Pat Cavit <pcavit@gmail.com>
2013-06-25 23:57:40 -0700

    First passing test

--- test/task-copy.js
+++ test/task-copy.js
@@ -2,10 +2,29 @@
 
 "use strict";
 
+var fs   = require("fs"),
+    path = require("path"),
+    exec = require("child_process").exec,
+    
+    assert = require("assert");
+    
+
 describe("Node web build", function() {
     describe("Task: Copy", function() {
-        it("should copy simple files", function(){
-            
+        it("should copy simple files", function(done) {
+            exec(
+                "node build.js -r test/specimens/simple copy",
+                function(error) {
+                    var dir = path.resolve(__dirname, "../temp/simple");
+                    
+                    assert.ifError(error);
+                    
+                    assert(fs.existsSync(dir));
+                    assert(fs.existsSync(path.join(dir, "test.js")));
+                    
+                    done();
+                }
+            );
         });
     });
 });
