a8b680d7608f552e377a5f8e00927ae4fd739de7
Pat Cavit <pcavit@gmail.com>
2013-06-25 22:44:06 -0700

    Start of testing + jshint updates for BDD

---
+++ test/task-copy.js
@@ -0,0 +1,11 @@
+/*jshint node:true */
+
+"use strict";
+
+describe("Node web build", function() {
+    describe("Task: Copy", function() {
+        it("should copy simple files", function(){
+            
+        });
+    });
+});
