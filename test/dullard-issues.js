90a761b5715ad474258daa92287d73f2ed50c53a
Pat Cavit <pcavit@gmail.com>
2014-01-20 11:31:42 -0800

    Add explicit test case for #30
    Fixes #30, also cleaned up some other test cases

--- test/dullard-issues.js
+++ test/dullard-issues.js
@@ -1,12 +1,14 @@
 /*jshint node:true */
 "use strict";
 
-var Dullard = require("../lib/dullard");
+var assert  = require("assert"),
+
+    Dullard = require("../lib/dullard");
 
 describe("Dullard", function() {
     describe("Dullard Class", function() {
         it("shouldn't confuse steps with Array.prototype methods (Issue #18)", function() {
-            var b1 = new Dullard({
+            var d1 = new Dullard({
                     dirs : [
                         "./test/specimens/tasks-other"
                     ],
@@ -16,7 +18,27 @@ describe("Dullard", function() {
                     ]
                 });
             
-            b1.run();
+            d1.run();
+        });
+
+        it("should use the task name as the log prefix (Issue #30)", function(done) {
+            var d1 = new Dullard({
+                   dirs : [
+                        "./test/specimens/tasks-a",
+                    ]
+                }),
+                prefixes = [];
+            
+            d1.on("log", function(args) {
+                prefixes.push(args.task);
+            });
+            
+            d1.run([ "a", "a-async" ], function() {
+                assert(prefixes.indexOf("a") > -1);
+                assert(prefixes.indexOf("a-async") > -1);
+                
+                done();
+            });
         });
     });
 });
